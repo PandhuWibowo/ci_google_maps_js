bbc8cff6f3ba83bc5a7c444e122dd6ea2126d35a
pandhuwibowo <pandhuw58@gmail.com>
2018-08-28 14:50:59 +0700

    Change into Vue JS services

--- maps/application/views/g_maps/index.php
+++ maps/application/views/g_maps/index.php
@@ -6,6 +6,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
   </head>
   <body>
+    <div id="app">
+
+
       <div class="form-group">
         <select class="varListPorpinsi form-control" name="varListPorpinsi" id="varListPorpinsi">
           <option value="0">Pilih Provinsi</option>
@@ -21,22 +24,30 @@
       </div>
 
       <div class="form-group">
-        <select class="varListKabko form-control" name="varListKabko" id="varListKabko">
+        <select class="varListKabko form-control" name="varListKabko" id="varListKabko" v-model="place">
           <option value="">Pilih Kabupaten/Kota</option>
         </select>
       </div>
-
-      <div id="info"></div>
-      <div id="googleMap" style="width:100%;height:380px;"></div>
+      <div class="col-md-6"><h3> Latitude : {{ latitude }}</h3></div>
+                      <div class="col-md-6"><h3> Longitude : {{ longitude }}</h3></div>
+      <div class="col-md-12"  v-bind:class="{ 'not-visible' : active }" >
+          <iframe frameborder="0" style="width: 100%; height: 350px; border:0" v-bind:src="'https://www.google.com/maps/embed/v1/place?key=AIzaSyCSdxjuCPhzR8BbQe3-crU3qoSC-_ymQBg&q='+ place+'&zoom=11'" allowfullscreen></iframe>
+      </div>
+      <!-- <div id="info"></div>
+      <div id="googleMap" style="width:50%;height:380px;"></div> -->
 
       <!-- <form action="" method="post">
         <input type="text" id="lat" name="lat" value="">
         <input type="text" id="lng" name="lng" value="">
       </form> -->
-
+</div>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" charset="utf-8"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.js" charset="utf-8"></script>
-      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDLaln3IEmJGbVRl7_1rkgwft1zEOMJ6fo&libraries=places&v=3.exp"></script>
+      <script src="https://unpkg.com/vue@2.0.3/dist/vue.js"></script>
+      <script src="https://unpkg.com/axios@0.12.0/dist/axios.min.js"></script>
+      <script src="https://unpkg.com/lodash@4.13.1/lodash.min.js"></script>
+      <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDJxBram_TlXO62Q0QzroUxP3Jq7BzKoRM&libraries=places&v=3.exp"></script> -->
+      <!-- <script src="http://maps.googleapis.com/maps/api/js"></script> -->
       <script type="text/javascript">
 
       $(function(){
@@ -59,42 +70,78 @@
         });
       });
 
-      var geocoder;
-      var map;
-      var geoMarker;
+      // var geocoder;
+      // var map;
+      // var geoMarker;
+      //
+      // function initialize() {
+      //   var map = new google.maps.Map(
+      //     document.getElementById("googleMap"), {
+      //       center: new google.maps.LatLng(-6.21462, 106.84513),
+      //       zoom: 13,
+      //       mapTypeId: google.maps.MapTypeId.ROADMAP
+      //     });
+      //   geoMarker = new google.maps.Marker();
+      //   geoMarker.setPosition(map.getCenter());
+      //   geoMarker.setMap(map);
+      //
+      //   $("#varListKabko").change(function() {
+      //     var addr = ($('#varListKabko').val());
+      //
+      //     var geocoder = new google.maps.Geocoder();
+      //     geocoder.geocode({
+      //       'address': addr
+      //     }, function(results, status) {
+      //       if (status == google.maps.GeocoderStatus.OK) {
+      //         map.setCenter(results[0].geometry.location);
+      //         geoMarker.setOptions({
+      //           position: results[0].geometry.location,
+      //         });
+      //       } else {
+      //         alert("Something got wrong " + status);
+      //       }
+      //     });
+      //   });
+      //
+      // }
+      // google.maps.event.addDomListener(window, "load", initialize);
 
-      function initialize() {
-        var map = new google.maps.Map(
-          document.getElementById("googleMap"), {
-            center: new google.maps.LatLng(37.4419, -122.1419),
-            zoom: 13,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-          });
-        geoMarker = new google.maps.Marker();
-        geoMarker.setPosition(map.getCenter());
-        geoMarker.setMap(map);
-
-        $("#varListKabko").change(function() {
-          var addr = ($('#varListKabko').val());
-
-          var geocoder = new google.maps.Geocoder();
-          geocoder.geocode({
-            'address': addr
-          }, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-              map.setCenter(results[0].geometry.location);
-              geoMarker.setOptions({
-                position: results[0].geometry.location,
-              });
-            } else {
-              alert("Something got wrong " + status);
+      var app = new Vue({
+        el: '#app',
+        data: {
+          place: '',
+          latitude: '',
+          longitude: '',
+          active : true
+        },
+        watch: {
+          place: function() {
+            this.latitude = '';
+            this.longitude = '';
+            this.active = true;
+            if (this.place.length >= 3) {
+              this.active = false;
+              this.lookupCoordinates();
             }
-          });
-        });
-
-      }
-      google.maps.event.addDomListener(window, "load", initialize);
-
+          }
+        },
+        methods: {
+          lookupCoordinates: _.debounce(function() {
+            var app = this;
+            app.latitude = "Searching...";
+            app.longitude = "Searching...";
+            axios.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + app.place)
+                  .then(function (response) {
+                    app.latitude = response.data.results[0].geometry.location.lat;
+                    app.longitude = response.data.results[0].geometry.location.lng;
+                  })
+                  .catch(function (error) {
+                    app.latitude = "Invalid place";
+                    app.longitude = "Invalid place";
+                  })
+          }, 500)
+        }
+      });
       </script>
   </body>
 </html>
